344ce3e58e4701a60dd5751024753af336e0646b
Yosefgid <jossykasshun@yahoo.com>
2026-05-06 13:12:18 +0100

    feat: Add Database context with enum conversion

---
+++ NCRecordShop/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NCRecordShop.Models;
+
+namespace NCRecordShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {//Since genre is an enum with num values 
+            //if this is not used it will put the nums in the database as opposed to the string
+            modelBuilder.Entity<Album>(entity =>
+            {
+                entity.Property(album => album.Genre).HasConversion<string>();
+            });
+        }
+    }
+}
--- NCRecordShop/Models/Album.cs
+++ NCRecordShop/Models/Album.cs
@@ -2,13 +2,13 @@
 {
     public class Album
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Artist { get; set; } = "";
         public int ReleaseYear { get; set; }
         public Genre Genre { get; set; }
         public int Quantity { get; set; }
-        public DateTime DateAdded {get; set;}
+        public DateTime DateAdded { get; set; } = DateTime.UtcNow;
 
     }
 }
