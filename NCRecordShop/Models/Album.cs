9204d4083661a55b9a439b788a2d6ceb51dab70e
Yosefgid <jossykasshun@yahoo.com>
2026-05-06 12:59:17 +0100

    feat: add Album class

---
+++ NCRecordShop/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿namespace NCRecordShop.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = "";
+        public string Artist { get; set; } = "";
+        public int ReleaseYear { get; set; }
+        public Genre Genre { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateAdded {get; set;}
+
+    }
+}
---
+++ NCRecordShop/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿namespace NCRecordShop.Models
+{
+    public enum Genre
+    {
+        RnB = 1,
+        House = 2,
+        Soul = 3,
+        Blues = 4, 
+        HipHop = 5,
+        Rock = 6,
+        Pop = 7,
+        Indie = 8,
+
+    }
+}
