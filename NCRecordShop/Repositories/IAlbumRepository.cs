67527d0473f7d79bf090fcf62979531e9f33540b
Yosefgid <jossykasshun@yahoo.com>
2026-05-06 13:46:52 +0100

    feat: Add IAlbumRepo with associated methods to be used for the AlbumRepo layer

---
+++ NCRecordShop/Repositories/IAlbumRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using NCRecordShop.Models;
+
+namespace NCRecordShop.Repositories
+{
+    public interface IAlbumRepository
+    {
+        List<Album> GetAllAlbums();
+        Album GetAlbumById(int id);
+        Album AddAlbum(Album album);
+        Album UpdateAlbum(int id, Album album);
+        bool DeleteAlbum(int id);
+    }
+}
