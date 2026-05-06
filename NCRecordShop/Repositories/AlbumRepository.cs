8104a8ba164e33f320efaa76df2ce31355e14608
Yosefgid <jossykasshun@yahoo.com>
2026-05-06 13:54:06 +0100

    feat: add the Repolayer with notImplemntedEx

---
+++ NCRecordShop/Repositories/AlbumRepository.cs
@@ -0,0 +1,35 @@
+﻿using NCRecordShop.Data;
+using NCRecordShop.Models;
+
+namespace NCRecordShop.Repositories
+{
+    public class AlbumRepository : IAlbumRepository
+    {
+        public readonly AppDbContext _context;
+        public AlbumRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Album> GetAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+        public  Album GetAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Album AddAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+        public Album UpdateAlbum(int id, Album album)
+        {
+            throw new NotImplementedException();
+        }
+       public bool DeleteAlbum(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
