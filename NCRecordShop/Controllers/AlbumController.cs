8842c6753460213c1ee8ec885e3db15f82d11d78
Yosefgid <jossykasshun@yahoo.com>
2026-05-17 16:50:38 +0100

    feat: add Controller layer with GetAllAlbums endpoint

---
+++ NCRecordShop/Controllers/AlbumController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NCRecordShop.Models;
+using NCRecordShop.Services;
+
+namespace NCRecordShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumServices _service;
+        public AlbumController(IAlbumServices service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public IActionResult GetAllAlbum()
+        {
+            var albums = _service.GetAllAlbums;
+            return Ok(albums);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public IActionResult AddAlbum(Album album)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateAlbum(int id, Album album)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAlbum(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecordShop.Tests/UnitTests/ctrl/AlbumControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NCRecordShop.Controllers;
+using NCRecordShop.Models;
+using NCRecordShop.Services;
+using NUnit.Framework;
+
+namespace RecordShop.Tests;
+
+public class AlbumControllerTests
+{
+    private Mock<IAlbumServices> _mockServices;
+    private AlbumController _controller;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockServices = new Mock<IAlbumServices>();
+        _controller = new AlbumController(_mockServices.Object);
+    }
+
+    [Test]
+    public void GetAll_Albums_Returns_OK()
+    {
+        //arrange
+        var album = new List<Album>
+        {
+            new Album
+            {
+            Title = "Son of Spergy",
+            Artist = "Daniel Caesar",
+            ReleaseYear = 2025,
+            Genre = Genre.RnB,
+            Quantity = 4
+            }
+        };
+
+        _mockServices.Setup(s => s.GetAllAlbums()).Returns(album);
+
+
+        //act
+        var result = _controller.GetAllAlbum();
+        //assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+
+
+    }
+
+}
