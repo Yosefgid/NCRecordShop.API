895e3ae091ab07b12c4bb493b9f223faf6012867
Yosefgid <jossykasshun@yahoo.com>
2026-05-19 12:06:59 +0100

    feat: add Album mode, RecodSummary and List components - add Temp album page

---
+++ NCRecordShop.Blazor/NCRecordShop.Blazor/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿namespace NCRecordShop.Blazor.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Artist { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+
+
+    }
+   
+}
